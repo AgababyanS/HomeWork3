2df624913a736183df02193581e55261082a7a14
Sergey Agababyan <agababyan.sergej@mail.ru>
2023-02-23 17:59:57 +0100

    Поправка задачи seminar3

--- seminar3/Program.cs
+++ seminar3/Program.cs
@@ -1,15 +1,15 @@
 ﻿Console.WriteLine("Напишите номер четверти");
-int a = int.Parse(Console.Readline());
-Console.WriteLine("x");
-int x = int.Parse(Console.Readline());
-Console.WriteLine("y");
-int y = int.Parse(Console.Readline());
+int a = int.Parse(Console.ReadLine());
+// Console.WriteLine("x");
+// int x = int.Parse(Console.ReadLine());
+// Console.WriteLine("y");
+// int y = int.Parse(Console.ReadLine());
 if (a==1){
-Console.WriteLine(x>0 && y>0);
-}if (a==2){
-Console.WriteLine(x<0 && y>0);
+Console.WriteLine("x>0 and y>0");
+}else if (a==2){
+Console.WriteLine("x<0 and y>0");
 }else if (a==3){
-Console.WriteLine(x<0 && y<0);
+Console.WriteLine("x<0 and y<0");
 }else if (a==4)
-Console.WriteLine(x>0 && y<0);
+Console.WriteLine("x>0 and y<0");
 
