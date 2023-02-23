70aa8cd706a596137d917383be0e06bb6d22b6f4
Sergey Agababyan <agababyan.sergej@mail.ru>
2023-02-23 18:14:12 +0100

    aaaa

--- zadach-test/Program.cs
+++ zadach-test/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 int i = int.Parse(Console.ReadLine()??"0");
+Console.WriteLine($"{i}Hello, World!");
